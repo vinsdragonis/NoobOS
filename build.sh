726931f5df3e73e4e1d4640679b456c6a67ae648
vinsdragonis <zrexteam128@gmail.com>
2022-07-23 13:31:52 +0530

    Placed binaries in different directory

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 nasm -f elf32 boot.asm -o boot.o
 gcc -m32 -c kernel.c -o kernel.o
 gcc -m32 -c ./src/print.c -o ./src/print.o
-ld -m elf_i386 -T link.ld -o kernel.bin boot.o kernel.o ./src/print.o
-qemu-system-i386 -kernel kernel.bin --curses
+ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
+qemu-system-i386 -kernel ./bin/kernel.bin --curses
--- kernel.c
+++ kernel.c
@@ -2,6 +2,6 @@
 
 void kernel_main()
 {
+    // Simple usage of print_msg() to print a string to the screen
     print_msg("kernel_main() has been called!");
-    // print_msg("print_msg() has been called!");
 }
