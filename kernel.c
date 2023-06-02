73a7e6354fad956a14de36ca4593a9fdc9c51fbe
Vineeth B V <37930821+vinsdragonis@users.noreply.github.com>
2023-06-02 11:48:35 +0530

    Update kernel.c

--- kernel.c
+++ kernel.c
@@ -1,21 +1,21 @@
 #include "./include/print.h"
-#include "./include/keyboard.h"
+// #include "./include/keyboard.h"
 
 void kernel_main() {
     // Initialize keyboard device
-    KBD kbd;
-    kbd_init(&kbd);
-    char key = 0;
-    __asm__ __volatile__(
-        "int $0x16\n"
-        : "=al"(key)
-    );
+//     KBD kbd;
+//     kbd_init(&kbd);
+//     char key = 0;
+//     __asm__ __volatile__(
+//         "int $0x16\n"
+//         : "=al"(key)
+//     );
     // Simple usage of print_msg() to print a string to the screen
     clear_screen();
     print_msg("kernel_main() has been called!\n");
     print_msg("print_msg() has been called!\n");
     // Print keyboard input to the screen
-    if (key != 0) {
-        kbd_handle_key_press(&kbd, key);
-    }
+//     if (key != 0) {
+//         kbd_handle_key_press(&kbd, key);
+//     }
 }
