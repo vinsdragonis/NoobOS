3c9b0e1ade4d10bd00f11ec2417a4d6d4646864b
vinsdragonis <zrexteam128@gmail.com>
2023-06-02 13:50:17 +0530

    Fixed issues with ld and qemu through build.sh

--- build.sh
+++ build.sh
@@ -1,14 +1,5 @@
 nasm -f elf32 boot.asm -o boot.o
-<<<<<<< Updated upstream
-gcc -m32 -c -lssp kernel.c -o kernel.o
-gcc -m32 -c -lssp ./src/print.c -o ./src/print.o
-# gcc -m32 -c -lssp ./src/keyboard.c -o ./src/keyboard.o
-# ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o ./src/keyboard.o
-ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
-qemu-system-x86_64 -kernel ./bin/kernel.bin
-=======
 gcc -m32 -c kernel.c -o kernel.o
 gcc -m32 -c ./src/print.c -o ./src/print.o
 ld -m elf_i386 -T link.ld -o ./bin/kernel.bin boot.o kernel.o ./src/print.o
 qemu-system-x86_64 -kernel ./bin/kernel.bin
->>>>>>> Stashed changes
--- kernel.c
+++ kernel.c
@@ -1,29 +1,8 @@
 #include "./include/print.h"
-<<<<<<< Updated upstream
-// #include "./include/keyboard.h"
 
 void kernel_main() {
-    // Initialize keyboard device
-//     KBD kbd;
-//     kbd_init(&kbd);
-//     char key = 0;
-//     __asm__ __volatile__(
-//         "int $0x16\n"
-//         : "=al"(key)
-//     );
-=======
-
-void kernel_main() {
->>>>>>> Stashed changes
     // Simple usage of print_msg() to print a string to the screen
     clear_screen();
     print_msg("kernel_main() has been called!\n");
     print_msg("print_msg() has been called!\n");
-<<<<<<< Updated upstream
-    // Print keyboard input to the screen
-//     if (key != 0) {
-//         kbd_handle_key_press(&kbd, key);
-//     }
-=======
->>>>>>> Stashed changes
 }
